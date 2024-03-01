e7156e7ed6abb31d0b5fe32d4f0b4b43cff0bd55
faketri <59346366+faketri@users.noreply.github.com>
2024-03-01 23:49:04 +0300

    Update Product.kt

--- src/main/kotlin/entity/Product.kt
+++ src/main/kotlin/entity/Product.kt
@@ -7,17 +7,12 @@ import lombok.*
 data class Product(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private var idProduct: Long? = null,
-    @Column
-    private var name: String,
-    @Column
-    private var price: Double?,
-    @Column
-    private var weight: Float?,
-    @Column
-    private var description: String?,
+    var idProduct: Long? = null,
+    var name: String,
+    var price: Double?,
+    var weight: Float?,
+    var description: String?,
     @OneToOne(cascade = arrayOf(CascadeType.ALL))
-    private var category: Category,
-    @Column(columnDefinition = "bytea")
-    private var imageProduct: ByteArray?
+    var category: Category,
+    var imageProduct: ByteArray?
 )

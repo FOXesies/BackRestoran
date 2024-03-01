9989c442f3a8466fe9b660de5b81b4f3e86b4017
faketri <59346366+faketri@users.noreply.github.com>
2024-03-01 23:45:06 +0300

    Update OrganizationRepository.kt

--- src/main/kotlin/repository/OrganizationRepository.kt
+++ src/main/kotlin/repository/OrganizationRepository.kt
@@ -4,7 +4,7 @@ import org.example.entity.Organization
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
-@Repository
+
 interface OrganizationRepository : JpaRepository<Organization, Long> {
 
 }
